44810d9261f3bafe7121551520ca3d34126f77a8
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-06-23 23:37:22 +0300

    fix: исправлены ошибки отображения шаблона. Исправлены загрузки статических файлов. Исправлена ошибка в settings.py: была повторяющаяся строчка STATIC_URL

--- Istok/Istok/orders/urls.py
+++ Istok/Istok/orders/urls.py
@@ -3,6 +3,6 @@ from .views import current_orders, order_history, order_detail
 
 urlpatterns = [
     path('current-orders/', current_orders, name='current_orders'),
-    path('order-history/', order_history, name='order_history'),
-    path('order/<int:pk>/', order_detail, name='order_detail'),
+    path('orders-history/', order_history, name='order_history'),
+    path('orders/<int:pk>/', order_detail, name='order_detail'),
 ]

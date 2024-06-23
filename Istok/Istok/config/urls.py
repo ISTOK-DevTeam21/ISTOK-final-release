1d337a056bab9b13d694966b8def29d4af383014
COBA_B_HOCKAX <mixeev.danila@yandex.ru>
2024-06-23 21:46:16 +0300

    feat: сделан шаблон base.html, для удобного использования в других шаблонах

--- Istok/Istok/config/urls.py
+++ Istok/Istok/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_page.urls')),  # добавляем пустой путь для main_page
-    path('current-orders/', include('orders.urls')),
+    path('', include('orders.urls')),
     path('appointments/', include('appointments.urls')),
     path('catalog/', include('catalog.urls')),
     path('about/', include('about_us.urls')),
